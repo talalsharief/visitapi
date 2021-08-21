f630af016de0e309e9e96572eaa46ef180b1b94c
talalsharief <talalsharieftalal@yahoo.com>
2021-08-21 15:05:03 +0500

    visitlogapi

---
+++ VisitLog_API/BusinessHelpers/ActivityHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VisitLogDataAccess;
+
+namespace VisitLog_API.BusinessHelpers
+{
+    public class ActivityHelper
+    {
+        public static int SaveActivity(int? UserID, int? PKID, string remarks, Enum activity)
+        {
+            TblActivity objTblActivity = new TblActivity();
+            if (UserID > 0)
+                objTblActivity.UserID = UserID;
+            objTblActivity.ActivityCode = activity.ToString();
+            if(PKID > 0)
+                objTblActivity.Pkid = PKID;
+            objTblActivity.Remarks = remarks;
+            objTblActivity.Save();
+
+            return objTblActivity.ActivityLogID;
+        }
+
+        public enum Activity
+        {
+            SyncMeterReading,
+            FetchMeterIndexinConfiguration,
+            FetchConsumer,
+            FetchMeter,
+            FillAppMeterStatus,
+            User,
+        }
+    }
+}
---
+++ VisitLog_API/Controllers/UserController.cs
@@ -0,0 +1,67 @@
+﻿using Common.DSMARTSecurity;
+using Newtonsoft.Json;
+using SmartCBS_API.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using VisitLog_API.BusinessHelpers;
+using VisitLogDataAccess;
+
+namespace VisitLog_API.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")] // tune to your needs
+    [RoutePrefix("api/user/userlogin")]
+    public class UserController : ApiController
+    {
+        //Start UserLogin
+        [Route("api/user/userlogin")]
+        [HttpGet]
+        public ResponseHelper DemoLogin([FromUri] clUser data)
+        {
+            string JSONDAta = "";
+            int ActivityID = 0;
+
+            ActivityID = ActivityHelper.SaveActivity(0, 0, "UserLogin", ActivityHelper.Activity.User);
+            try
+            {
+                UserLoginCollection objTblUserCol = new UserLoginCollection()
+                    .Where(UserLogin.Columns.UserName, data.username)
+                    .Where(UserLogin.Columns.Passworrd, DSMARTSecurity.EncryptData(data.password))
+                    .Load();
+                var result = objTblUserCol.Select(x => new
+                {
+                    username = x.UserName,
+                });
+
+                if (objTblUserCol.Count > 0)
+                {
+
+                    JSONDAta = JsonConvert.SerializeObject(result);
+                    return new ResponseHelper(1, result, new ErrorDef((int)EnumHelper.ErrorEnums.NoRecordFound, "Error", EnumHelper.ErrorEnums.NoRecordFound.ToString(), "Error"));
+                }
+                else
+                {
+                    JSONDAta = JsonConvert.SerializeObject(result);
+                    return new ResponseHelper(0, new object(), new ErrorDef());
+                }
+
+            }
+            catch (Exception ex)
+            {
+                BaseHelper.ExceptionLogging("DemoLogin", ex.Message, "DemoLogin", ex.Message, 0);
+                return new ResponseHelper(0, new object(), new ErrorDef((int)EnumHelper.ErrorEnums.InvalidData, "Error", EnumHelper.ErrorEnums.Exception.ToString(), "This data is Incorrect"));
+            }
+            finally
+            {
+                string JSONDevice = JsonConvert.SerializeObject(data.deviceinfo);
+
+                BaseHelper.SystemLogging(0, "api/user/userlogin", JSONDAta, JSONDevice);
+            }
+        }
+        //End UserLogin
+    }
+}
